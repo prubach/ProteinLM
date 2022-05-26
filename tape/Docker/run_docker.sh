3f118a032d9259c10f5852acde6c89320a1924f8
Paweł Rubach <pawel.rubach@gmail.com>
2022-05-26 17:46:39 +0200

    Added script to run docker image interactively

--- tape/Docker/run_docker.sh
+++ tape/Docker/run_docker.sh
@@ -1,1 +1,7 @@
-docker run --gpus all -v `pwd`../:/io -it pytorch_proteinlm:latest bash
+# run from ProteinLM/tape using:
+# bash Docker/run_docker.sh
+# once starts invoke:
+# conda activate torch1.7
+# cd /io
+# Now you should be ready to run megatron scripts
+docker run --gpus all -v `pwd`:/io -it pytorch_proteinlm:latest bash
