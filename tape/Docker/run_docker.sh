a11a46131fb0752192ec5d0ff727af8797e47351
Paweł Rubach <pawel.rubach@gmail.com>
2022-05-27 01:20:47 +0200

    Working on PyTorch environment for megatron

--- tape/Docker/run_docker.sh
+++ tape/Docker/run_docker.sh
@@ -4,4 +4,6 @@
 # conda activate torch1.7
 # cd /io
 # Now you should be ready to run megatron scripts
-docker run --gpus all -v `pwd`:/io -it pytorch_proteinlm:latest bash
+
+docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v `pwd`/..:/io -it pytorch_proteinlm:latest bash
+#docker run --gpus all
